0408836c22fdb4053d08607efca9db5dce0dbf3b
skybound0532 <tianchenggu@outlook.com>
2023-10-26 14:14:25 -0500

    Test Square Class

--- src/SquareTester.java
+++ src/SquareTester.java
@@ -1,9 +1,12 @@
 public class SquareTester
 {
-    Square test = new Square(1,2,3);
-    System.out.println(test);
-    System.out.println(test.getRow());
-    System.out.println(test.getCol());
+    public static void main(String[] args)
+    {
+        Square test = new Square(1,2,3);
+        System.out.println(test);
+        System.out.println(test.getRow());
+        System.out.println(test.getCol());
+    }
 
     //
 }
