e9ece964300ff88ee8188bde4f8b3d3473642dd4
skybound0532 <tianchenggu@outlook.com>
2023-10-26 14:13:13 -0500

    Add SquareTester

---
+++ src/SquareTester.java
@@ -0,0 +1,9 @@
+public class SquareTester
+{
+    Square test = new Square(1,2,3);
+    System.out.println(test);
+    System.out.println(test.getRow());
+    System.out.println(test.getCol());
+
+    //
+}
