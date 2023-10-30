bce65dbbc41d3722326db9f8a07d02dc5be2f5e0
akhil-kanuri <111461772+akhil-kanuri@users.noreply.github.com>
2023-10-30 13:43:56 -0500

    Completed MazeTest branch

--- src/MazeTest.java
+++ src/MazeTest.java
@@ -3,11 +3,24 @@ public class MazeTest
 {
     public static void main(String[] args)
     {
-        Maze maze = null;;
+        Maze maze = new Maze();
         maze.loadMaze("maze-1");
-        ArrayList <Square> sq = new ArrayList<>();
-        maze.getNeighbors(Square sq);
+        Square square = maze.getStart();
+        System.out.println(square.getRow());
+        System.out.println(square.getCol());
+        System.out.println(square);
+        ArrayList<Square> sq = new ArrayList<>();
+        sq = maze.getNeighbors(square);
+
+        for (Square s: sq)
+        {
+            System.out.println(s);
+        }
         //maze.getStart();
-        //maze.getFinish();
+        square = maze.getFinish();
+
+        System.out.println(square.getRow());
+        System.out.println(square.getCol());
+        System.out.println(square);
     }
 }  
